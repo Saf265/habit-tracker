0207c8e3a5c60ec42635c9aa73a3f5434a022df2
Saf2339 <boucetasafwan@gmail.com>
2024-10-24 12:03:13 +0200

    Habit cron Commit

---
+++ app/api/resetHabit/route.ts
@@ -0,0 +1,35 @@
+// /app/api/resetHabit/route.ts
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  try {
+    // 1. Récupérer toutes les habitudes actuelles
+    const habits = await prisma.habit.findMany();
+
+    // 2. Créer une nouvelle entrée dans HistoryHabit pour chaque habitude
+    const historyData = habits.map((habit) => ({
+      habitId: habit.id,
+      completed: habit.completed,
+    }));
+
+    await prisma.historyHabit.createMany({
+      data: historyData,
+    });
+
+    // 3. Réinitialiser les habitudes (mettre completed à false)
+    await prisma.habit.updateMany({
+      data: { completed: false },
+    });
+
+    return NextResponse.json({
+      message: "Habits have been reset and history saved!",
+    });
+  } catch (error) {
+    console.error("Error resetting habits or saving history:", error);
+    return NextResponse.json(
+      { error: "Failed to reset habits or save history" },
+      { status: 500 }
+    );
+  }
+}
